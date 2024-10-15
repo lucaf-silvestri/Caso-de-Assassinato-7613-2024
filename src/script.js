a10a47e7a01b8e34bbcbc2eea5f9d1bb60d301f6
Luca Figliola Silvestri <lucaf.silvestri@yahoo.com.br>
2024-10-14 21:28:02 -0300

    Cadastro 4

--- src/script.js
+++ src/script.js
@@ -1,15 +1,3 @@
-const profileImg = document.getElementById('profile-img');
-const dropdownMenu = document.getElementById('dropdown-menu');
-
-profileImg.addEventListener('click', function () {
-    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
-});
-window.addEventListener('click', function (event) {
-    if (!profileImg.contains(event.target) && !dropdownMenu.contains(event.target)) {
-        dropdownMenu.style.display = 'none';
-    }
-});
-
 // Banco de Dados
 
 // CADASTRO:
